45cf12b83e033f8abc4c90802841be670ed9a94b
hyoungdong <qazsw13@naver.com>
2024-03-19 11:23:06 +0900

    Remove all files for a clean state

--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-AWS_ACCESS_KEY = "" 
-AWS_SECRET_KEY = "" 
-aws_account_id = ""
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_SECRET_KEY" {}
-variable "aws_account_id" {
-  description = "The AWS Account ID"
-  type        = string
-}
-
